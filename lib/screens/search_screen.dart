af26a609fb88692443c1fbda6ff6136e25f8db51
MaryamHam <107949861+MaryamHam@users.noreply.github.com>
2023-09-17 19:51:59 +0400

    search textField

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -9,9 +9,39 @@ class SearchScreen extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 209, 191, 240),
         appBar: AppBar(title: Text("Search Screen"),),
 
-        body:Container(
-        margin: EdgeInsets.all(30),
-        child: Text("Search" , style: TextStyle(fontSize: 40 ,color: Colors.deepPurple,fontWeight: FontWeight.bold))),
+        body:Column(
+          children: [
+            Container(
+            margin: EdgeInsets.all(30),
+            child: Text("Search" , style: TextStyle(fontSize: 40 ,color: Colors.deepPurple,fontWeight: FontWeight.bold))
+            ),
+
+            Container(
+              margin: EdgeInsets.all(20),
+              child:  TextFormField(
+        validator: (value){
+     
+        },
+        
+      style: TextStyle(color: Colors.deepPurple),
+      decoration: InputDecoration(
+      filled: true,
+      fillColor:Color.fromARGB(61, 255, 255, 255),
+      hintText: "search for product",
+      prefixIcon: Icon(Icons.search, color: Colors.purple),            
+      hintStyle: TextStyle(
+      color: Colors.purple,
+      ),
+    
+          focusedBorder:OutlineInputBorder(
+            borderSide: BorderSide(width: 2, color: Colors.white),
+            
+          ), 
+      
+          ), ),
+            )
+          ],
+        ),
   
 
     );
